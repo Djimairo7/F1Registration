3dc1f9793ad4a84fc95d944d82b6ca2d0ef82ea7
djimairo <djimairo2007@hotmail.com>
2023-12-27 18:34:06 +0100

    trying to change login front end
    need to fix it for mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="container p-3">
-            <div class="row justify-content-center">
-                <div class="col-md-6 bg-black text-white rounded overflow-hidden">
-                    <div class="p-3">
-                        
-                        <form action="" method="post" enctype="multipart/form-data" class="p-3">
-                            @csrf
-                            <input type="file" name="image" class="d-none"/>
+<div class="d-flex flex-column justify-content-center align-items-center vh-100">
+    <div class="container p-3">
+        <div class="row justify-content-center">
+            <div class="col-md-6 bg-black text-white rounded overflow-hidden">
+                <div class="p-3">
 
-                            <div class="form-group mb-3">
-                                <label for="first_name" class="text-white">
-                                    Voornaam
-                                </label>
-                                <input type="text" name="first_name" class="form-control bg-secondary text-white"/>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="last_name" class="text-white">
-                                    Achternaam
-                                </label>
-                                <input type="text" name="last_name" class="form-control bg-secondary text-white"/>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="bio" class="text-white">
-                                    Over mij
-                                </label>
-                                <textarea rows="4" name="bio" class="form-control bg-secondary text-white">
-                            </textarea>
-                            </div>
-                            <button type="submit" class="btn btn-danger w-25 align-items-center d-flex justify-content-center mx-auto">
-                                opslaan
-                            </button>
-                        </form>
-                    </div>
+
+                    <form action="" method="post" enctype="multipart/form-data" class="p-3">
+                        @csrf
+
+
+                        <div class="form-group mb-3">
+                            <label for="email" class="text-white">
+                                Email
+                            </label>
+                            <input type="text" name="email" class="form-control bg-secondary text-white"
+                                placeholder="Vul hier je email in">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="first_name class=text-white">
+                                Voornaam
+                            </label>
+                            <input type="text" name="first_name" class="form-control bg-secondary text-white"
+                                placeholder="Vul je voornaam in">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="last_name" class="text-white">
+                                Achternaam
+                            </label>
+                            <input type="text" id="last_name" name="last_name"
+                                class="form-control bg-secondary text-white" placeholder="Vul je Achternaam in">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password" class="text-white">
+                                Wachtwoord
+                            </label>
+                            <input type="text" id="password" name="password"
+                                class="form-control bg-secondary text-white" placeholder="Vul je wachtwoord in">
+                        </div>
+
                 </div>
+                <button type="submit"
+                    class="btn btn-danger w-25 align-items-center d-flex justify-content-center mx-auto">
+                    opslaan
+                </button>
+                </form>
             </div>
         </div>
     </div>
+</div>
+</div>
 
 
 @endsection
