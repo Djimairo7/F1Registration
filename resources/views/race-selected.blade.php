ad8d8e22bf86a3eb86459b764ff7a4a46b9db639
djimairo <djimairo2007@hotmail.com>
2024-01-02 12:09:43 +0100

    race selected ok
    mid

---
+++ resources/views/race-selected.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<div class="d-flex flex-column justify-content-center align-items-center vh-100">
+    <div class="container p-3">
+        <div class="row justify-content-center">
+            <div class="col-md-12 bg-secondary text-white rounded overflow-hidden">
+                <div class="p-3">
+                    <div class="mb-6">
+
+                        <h5 class="">Racenaam:</h5>
+                        <h5>Locatie:</h5>
+                        <h5>Datum:</h5>
+                        <br>
+                        <h5>Tijd</h5>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-3">
+        <div class="row justify-content-center">
+            <div class="col-md-12 bg-secondary text-white rounded overflow-hidden">
+                <div class="p-3">
+                    <div class="text-center mb-6">
+                        <div class="d-flex flex-row">
+                            <div class="container p-3">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-12 mt-3 bg-black text-white rounded overflow-hidden">
+                                        <div class="p-3">
+                                            <div class="text-center mb-6">
+                                                <h5>Naam:</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container p-3">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-12 bg-black text-white rounded overflow-hidden">
+                                        <div class="p-3">
+                                            <div class="text-center mb-6">
+                                                <h5>Naam:</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container p-3">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-12 mt-3 bg-black text-white rounded overflow-hidden">
+                                        <div class="p-3">
+                                            <div class="text-center mb-6">
+                                                <h5>Naam:</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div class="container p-3">
+                            <div class="row justify-content-center">
+                                <div class="col-md-12 mt-3 bg-black text-white rounded overflow-hidden">
+                                    <div class="p-3">
+                                        <div class="text-center mb-6">
+
+                                            <div class="container p-2">
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-12 bg-secondary text-white rounded overflow-hidden">
+                                                        <div class="p-3">
+                                                            <div class="text-center mb-2">
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="container p-2">
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-12 bg-dark text-white rounded overflow-hidden">
+                                                        <div class="p-3">
+                                                            <div class="text-center mb-2">
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="container p-2">
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-12 bg-secondary text-white rounded overflow-hidden">
+                                                        <div class="p-3">
+                                                            <div class="text-center mb-2">
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="container p-2">
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-12 bg-dark text-white rounded overflow-hidden">
+                                                        <div class="p-3">
+                                                            <div class="text-center mb-2">
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+
+
+
+
+@endsection
