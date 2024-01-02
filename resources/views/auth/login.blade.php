863b2a5c3e356b9e3b6fe5e3e55b6c16a32bf31b
djimairo <djimairo2007@hotmail.com>
2024-01-02 13:03:03 +0100

    register fixed to fit mockup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,39 +4,29 @@
 <div class="d-flex flex-column justify-content-center align-items-center vh-100">
     <div class="container p-3">
         <div class="row justify-content-center">
-            <div class="col-md-6 bg-black text-white rounded overflow-hidden">
+            <div class="col-md-6 bg-dark text-white rounded overflow-hidden">
                 <div class="p-3">
-                    <style>
-                        body {
-                            overflow: hidden;
-                        }
-                    </style>
-                    <script src="../../views/resources/js/login.js"></script>
-
-
+                    <h1 class="text-danger text-center">Login</h1>
                     <form action="" method="post" enctype="multipart/form-data" class="p-3">
                         @csrf
-
 
                         <div class="form-group mb-3">
                             <label for="email" class="text-white">
                                 Email
                             </label>
-                            <input type="text" name="email" class="form-control bg-secondary text-white"
-                                placeholder="Vul hier je email in">
+                            <input type="text" name="email" class="form-control bg-secondary text-white" placeholder="Vul hier je email in">
                         </div>
                         <div class="form-group mb-3">
                             <label for="password" class="text-white">
                                 Wachtwoord
                             </label>
-                            <input type="text" id="password" name="password"
-                                class="form-control bg-secondary text-white" placeholder="Vul je wachtwoord in">
+                            <input type="text" id="password" name="password" class="form-control bg-secondary text-white" placeholder="Vul je wachtwoord in">
                             <div class="container p-3 d-flex justify-content-center align-items-end">
-                                <a href="/register" class="text-danger font-weight-bold text-decoration-none">Nog geen
+                                <a href="/register" class="text-white font-weight-bold text-decoration-none">Nog geen
                                     account? Registreer hier</a>
                             </div>
                             <div class="container p-3 d-flex justify-content-center">
-                                <a href="/placeholder" class="text-danger font-weight-bold text-decoration-none">
+                                <a href="/placeholder" class="text-white font-weight-bold text-decoration-none">
                                     Wachtwoord Vergeten?
 
                                 </a>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,16 +4,11 @@
 <div class="d-flex flex-column justify-content-center align-items-center vh-100">
     <div class="container p-3">
         <div class="row justify-content-center">
-            <div class="col-md-6 bg-black text-white rounded overflow-hidden">
+            <div class="col-md-6 bg-dark text-white rounded overflow-hidden">
                 <div class="p-3">
-                    <style>
-                        body {
-                            overflow: hidden;
-                        }
-                    </style>
+                    <h1 class="text-danger text-center">Registreer</h1>
 
                     <body>
-
 
                         <form action="" method="post" enctype="multipart/form-data" class="p-3">
                             @csrf
@@ -21,35 +16,31 @@
                                 <label for="username class=text-white">
                                     Gebruikersnaam
                                 </label>
-                                <input type="text" name="username" class="form-control bg-secondary text-white"
-                                    placeholder="Vul hier je gebruikersnaam in">
+                                <input type="text" name="username" class="form-control bg-secondary text-white" placeholder="Vul hier je gebruikersnaam in">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="email" class="text-white">
                                     Email
                                 </label>
-                                <input type="text" name="email" class="form-control bg-secondary text-white"
-                                    placeholder="Vul hier je email in">
+                                <input type="text" name="email" class="form-control bg-secondary text-white" placeholder="Vul hier je email in">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="password" class="text-white">
                                     Wachtwoord
                                 </label>
-                                <input type="text" id="password" name="password"
-                                    class="form-control bg-secondary text-white"
-                                    placeholder="Vul hier je wachtwoord in">
+                                <input type="text" id="password" name="password" class="form-control bg-secondary text-white" placeholder="Vul hier je wachtwoord in">
+                                <button type="submit" class="btn btn-danger mt-3 w-25 align-items-center d-flex justify-content-center mx-auto">
+                                    Registreer
+                                </button>
                                 <div class="container p-3 d-flex justify-content-center align-items-end">
-                                    <a href="/login" class="text-danger font-weight-bold text-decoration-none">Heb je al
+                                    <a href="/login" class="text-white font-weight-bold text-decoration-none">Heb je al
                                         een
                                         account? log hier in
                                     </a>
 
                                 </div>
                             </div>
-                            <button type="submit"
-                                class="btn btn-danger  w-25 align-items-center d-flex justify-content-center mx-auto">
-                                Registreer
-                            </button>
+
                         </form>
                 </div>
             </div>
