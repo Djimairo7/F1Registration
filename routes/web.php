10c17a63235ab5523fdf9c7de7296edecfe75c5e
djimairo <djimairo2007@hotmail.com>
2024-01-02 19:05:29 +0100

    back to what we had before

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('/profiles', 'profilescontroller@store')->name('profiles.store');
+// Route::post('/profiles', 'profilescontroller@store')->name('profiles.store');
