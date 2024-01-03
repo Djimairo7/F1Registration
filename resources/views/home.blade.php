8de3a566e0d00a2c4610fa86bae6e2da1521b421
Rick <S1204255@student.windesheim.nl>
2024-01-03 13:55:02 +0100

    Refactor home.blade.php view file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,21 @@
 @section('content')
     <div class="container bg-gray">
         <div class="row">
-            <!-- <div class="col-md-4">
-                                    <div class="card">
-                                        <div class="card-header">{{ __('Dashboard') }}</div>
+            {{-- <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                                        <div class="card-body">
-                                            @if (session('status'))
-    <div class="alert alert-success" role="alert">
-                                                {{ session('status') }}
-                                            </div>
-    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                                            {{ __('You are logged in!') }}
-                                        </div>
-                                    </div>
-                                </div> -->
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div> --}}
 
             <div class="col-md-4">
                 <div class="d-flex flex-wrap flex-md-column m-2">
