7bb8f2c631b36c2c005b2d5f9f0764ac15e3e45f
djimairo <djimairo2007@hotmail.com>
2024-01-10 13:53:40 +0100

    wacht wacht record updated succesfahley🈴

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
         $score = Score::findorFail($id);
         $score->delete();
         return redirect()->route('admin')
-            ->with('success', 'Profile deleted successfully.');
+            ->with('success', 'Record deleted successfully.');
     }
     public function edit()
     {
