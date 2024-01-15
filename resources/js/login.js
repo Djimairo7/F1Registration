19d74167cba82ce557d68ca14127df397c4ddebf
Rick Slierendregt <113343182+Pluers@users.noreply.github.com>
2024-01-15 09:42:04 +0100

    Delete resources/js/login.js

--- resources/js/login.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var inputUsername = document.getElementById("username");
-    var inputPassword = document.getElementById("password");
-    if (inputUsername.value === "" || inputPassword.value === "") {
-     ("Please fill in all fields.");
-      return false;
-    }
-}
