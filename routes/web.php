e58eed6d8d52f95559fa91f8bcf5600c083a4ee9
Rick <S1204255@student.windesheim.nl>
2024-01-16 11:13:12 +0100

    Refactor card margins and add delete functionality for notifications

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,14 @@ Route::get('/', function () {
 });
 Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/discover', [App\Http\Controllers\DiscoverController::class, 'index'])->name('discover');
+
+Route::delete('/notification/{id}', 'App\Http\Controllers\NotificationController@destroy')->name('notification.destroy');
+
 Route::get('/race/{raceName}', 'App\Http\Controllers\RaceController@show')->name('race.show');
 Route::post('/race/{raceName}/submit', 'App\Http\Controllers\RaceController@submitScore')->name('race.submit');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::post('/profiles', 'profilescontroller@store')->name('profiles.store');
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 Route::delete('/admin/delete/{id}', [AdminController::class, 'delete'])->name('admin.delete');
 Route::put('/admin/update/{id}', [AdminController::class, 'update'])->name('admin.update');
-
-
-
-// add aan het einde van de register routes later
-// ->middleware('guest');
