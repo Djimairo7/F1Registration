c85c4cd10e2b1e60461fca6e24cf76925d6c935e
Rick <S1204255@student.windesheim.nl>
2024-04-08 10:38:06 +0200

    Refactor code in AppServiceProvider.php to use instances instead of singletons

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -87,12 +87,17 @@ class AppServiceProvider extends ServiceProvider
             }
         }
 
-        $this->app->instance('races', $races['MRData']['RaceTable']['Races']);
-        $this->app->instance('drivers', $drivers['MRData']['DriverTable']['Drivers']);
-        $this->app->instance('currentRace', $currentRace);
-        view()->share('currentRace', $currentRace);
-        view()->share('raceImages', $raceImages);
-        view()->share('currentDate', $currentDate);
-        view()->share('currentRace', $currentRace);
+        // Share the variables with all views
+        $variables = [
+            'races' => $races['MRData']['RaceTable']['Races'],
+            'drivers' => $drivers['MRData']['DriverTable']['Drivers'],
+            'currentRace' => $currentRace,
+            'raceImages' => $raceImages,
+            'currentDate' => $currentDate,
+        ];
+        foreach ($variables as $name => $value) {
+            $this->app->instance($name, $value);
+            view()->share($name, $value);
+        }
     }
 }
